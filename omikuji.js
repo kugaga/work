9c136433d6ba2e128c0201ddc2c473f0a8385b6d
kugaga <kuuga20010912@gmail.com>
2022-07-19 18:05:30 +0900

    [Start]Webアプリケーションをつくってみよう-2

--- omikuji.js
+++ omikuji.js
@@ -2,6 +2,12 @@ let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
 
+if(username == ""){
+    document.getElementById("name").innerHTML = "名無し";
+}
+else {document.getElementById("name").innerHTML = username;
+}
+
 let rand = Math.floor( Math.random() * 5)
 if (rand == 0) {
     userresult = "大吉";
